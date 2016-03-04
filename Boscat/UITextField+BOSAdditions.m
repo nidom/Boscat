97e5a1e38a42780af652e657a5be76c69e12ae6f
nidom <207666@gmail.com>
2016-03-04 11:25:34 +0800

    test commit

--- Boscat/UITextField+BOSAdditions.m
+++ Boscat/UITextField+BOSAdditions.m
@@ -9,6 +9,11 @@
 #import "UITextField+BOSAdditions.h"
 #import "BOSConditionMaker.h"
 @implementation UITextField (BOSAdditions)
-
+开发-芮华建  10:35:38
+怎么看都像是小赵啊
+开发-秦强  10:36:54
+连续加班100多天
+开发-秦强  10:36:58
+好牛逼啊
 
 @end
